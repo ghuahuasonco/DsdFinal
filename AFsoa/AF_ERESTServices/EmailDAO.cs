1fabb265122db4df93a3387e25dea90f506ab815
Gilberto Huahuasonco Huamantuma <ghuahuasonco@gmail.com>
2015-02-15 22:40:20 -0600

    DsdFinal
    DsdFinal

---
+++ AFsoa/AF_ERESTServices/EmailDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace AF_ERESTServices
+{
+    public class EmailDAO
+    {
+        public Email Enviar(Email EnviarEmail) {
+            Email Emailenviado = null;
+            SmtpClient cliente = new SmtpClient("smtp.gmail.com", 587);
+            cliente.EnableSsl = true;
+            cliente.Timeout = 10000;
+            cliente.DeliveryMethod = SmtpDeliveryMethod.Network;
+            cliente.UseDefaultCredentials = false;
+            cliente.Credentials = new NetworkCredential("ghuahuasonco@gmail.com", "Kross986280383Kross");
+            MailMessage msg = new MailMessage();
+            msg.To.Add(EnviarEmail.msgto);
+            msg.From = new MailAddress("ghuahuasonco@gmail.com");
+            msg.Subject = EnviarEmail.msgsubjet;
+            msg.Body = EnviarEmail.msgbody;
+            msg.IsBodyHtml = true;
+            cliente.Send(msg);
+            return Emailenviado;
+        }
+    }
+}
---
+++ AFsoa/RESTtest/TPrograms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Linq;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace RESTtest
+{
+    [TestClass]
+    public class TPrograms
+    {
+        [TestMethod]
+        public void TProgramstest()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest
+                .Create("http://localhost:1266/TPrograms.svc/TPrograms"); req.Method = "GET";
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string tprogram = reader.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            List<TProgram> TPlista = js.Deserialize<List<TProgram>>(tprogram);
+
+        }
+    }
+}
